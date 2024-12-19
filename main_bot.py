8b1230c51093a79225c75386cd9f22c35e813d8f
1111 <asirisprog@gmail.com>
2024-12-19 13:35:30 +0300

    fail for starting bot

--- main_bot.py
+++ main_bot.py
@@ -1,19 +1,25 @@
+import os.path
+
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import logging
+from handlers import router
 import asyncio
+from get_weather_api import get_forecast_weather, get_forecast_weather_gor_n_days
+from data_for_plot import get_data_for_plot
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import pandas as pd
+
 
 API_TOKEN = '7986428820:AAHgPe3l-Ruj5DhHzjUiF_JEUCV4-Pb1b6I'
 
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
+dp.include_router(router)
 
-@dp.message(F.text == '/start')
-async def start_command(message: types.Message):
-    await message.answer(
-        'Добро пожаловать в бота погоды!'
-    )
 
 
 
